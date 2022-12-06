76aa53bc4bc6d735d22d1ab59d94be05d94a16c6
Cathy <cathyrebopas@gmail.com>
2022-12-06 00:23:30 -0300

    Funcionalidad página, ediciones en css y js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,46 @@
-$(function(){
-$('[data-bs-toggle="tooltip"]').tooltip();
+$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
 
- // click boton enviar
-$("#enviar").click(function () {
-    alert("El formulario fue enviado :)");
-});
+    // click boton enviar
+    $("#enviar").click(function () {
+        alert("El formulario fue enviado :)");
+    });
 })
 
 //scroll
-$(document).scroll(function(){
-const y = $("html").scrollTop();
-  
-y > 300 ? $("nav").addClass("nav-black") :$("nav").remvoveClass("nav-black");
+$(document).scroll(function () {
+    const y = $("html").scrollTop();
 
+    y > 500 ?
+    $("nav").addClass("nav-black")
+    :
+    $("nav").removeClass("nav-black");
 })
 
 //insertar datos//
 //Insertar correo electrónico//
 $("#nombre").click(function () {
-    prompt("Ingrese su nombre y apellido", "");
+    var nombre = prompt("Ingrese su nombre y apellido", "");
+    var element = document.getElementById("nombre")
+    if (nombre !== "") {
+        element.value = nombre
+    }
 });
 
 $("#asunto").click(function () {
-var tituloasunto = prompt("Ingrese el asunto", "");
+    var tituloasunto = prompt("Ingrese el asunto", "");
+    var titulo = document.getElementById("asunto")
+    if (tituloasunto !== "") {
+        titulo.value = tituloasunto
+    }
 });
-var titulo = document.getElementById ("asunto2")
-titulo.innetHTML = tituloasunto; //areglar//
+
+
 
 $("#exampleFormControlTextarea1").click(function () {
-    prompt("Ingrese  el texto del mensaje", "");
+    var text = prompt("Ingrese  el texto del mensaje", "");
+    var element2 = document.getElementById("exampleFormControlTextarea1")
+    if (text !== "") {
+        element2.value = text
+    }
 });
